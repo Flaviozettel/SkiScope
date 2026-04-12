d0fa6388ff16311ac915e611f69d997af474d9a5
mxelaniie <melanie.schiefermueller@students.fhnw.ch>
2026-04-12 11:08:48 +0200

    Zentroide  hinzugefügt

--- client/src/MainArea.jsx
+++ client/src/MainArea.jsx
@@ -540,7 +540,7 @@ export const MainArea = ({ aktivDatum, setAktivDatum }) => {
                         <circle cx="12" cy="12" r="10" />
                         <polyline points="12 6 12 12 16 14" />
                       </svg>
-                      Geöffnet
+                      Geöffnet provisorisch
                     </div>
 
                     {/* Lifte */}
