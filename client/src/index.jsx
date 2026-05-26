9176e1c01111a8d1d4595fd7a9e02b5fb5a610b0
Manuel Hubler <manuel.hubler2002@gmail.com>
2026-05-26 12:25:17 +0200

    index.jsx: kommentieren

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,3 +1,6 @@
+// Einstiegspunkt: React in <div id="root"> mounten.
+// StrictMode hilft beim Aufspüren von Side-Effects in Dev.
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.jsx";
